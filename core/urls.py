ac30ab1a614338541218dbc83127bd1a9abe4a5f
Paulo André Cardoso Silva Santos <pauloandrecss@gmail.com>
2024-05-23 19:28:04 -0300

    root url created

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda req: redirect('/home/')),
     path('admin/', admin.site.urls),
     path('home/', include('blog.urls'))
 ]
